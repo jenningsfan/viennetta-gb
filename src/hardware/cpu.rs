0ee8c3d727c00d3a16e2e1ce95b333c4f64f2edb
Sammy Humphreys <Sammyh2009@outlook.com>
2024-01-22 16:45:07 +0000

    Stub out all block 1 opcodes and implement most of them

--- src/hardware/cpu.rs
+++ src/hardware/cpu.rs
@@ -43,8 +43,8 @@ impl Registers {
         }
     }
     
-    fn get_r8(&self, opcode: u8, ram: &RAM) -> u8 {
-        match opcode {
+    fn get_r8(&self, reg: u8, ram: &RAM) -> u8 {
+        match reg {
             0 => self.b,
             1 => self.c,
             2 => self.d,
@@ -57,8 +57,8 @@ impl Registers {
         }
     }
 
-    fn set_r8(&mut self, opcode: u8, value: u8, ram: &mut RAM) {
-        match opcode {
+    fn set_r8(&mut self, reg: u8, value: u8, ram: &mut RAM) {
+        match reg {
             0 => self.b = value,
             1 => self.c = value,
             2 => self.d = value,
@@ -76,8 +76,13 @@ impl Registers {
         (self.h as u16) << 8 | self.l as u16
     }
 
-    fn get_r16(&self, opcode: u8) -> u16 {
-        match opcode {
+    fn set_hl(&mut self, value: u16) {
+        self.h = (value >> 8) as u8;
+        self.l = (value & 0x0F) as u8;
+    }
+
+    fn get_r16(&self, reg: u8) -> u16 {
+        match reg {
             0 => (self.b as u16) << 8 | self.c as u16,
             1 => (self.d as u16) << 8 | self.e as u16,
             2 => self.get_hl(),
@@ -87,8 +92,28 @@ impl Registers {
         }
     }
 
-    fn get_r16_stk(&self, opcode: u8) -> u16 {
-        match opcode {
+    fn set_r16(&mut self, reg: u8, value: u16) {
+        match reg {
+            0 => {
+                self.b = (value >> 8) as u8;
+                self.c = (value & 0x0F) as u8;
+            },
+            1 => {
+                self.d = (value >> 8) as u8;
+                self.e = (value & 0x0F) as u8;
+            },
+            2 => {
+                self.h = (value >> 8) as u8;
+                self.l = (value & 0x0F) as u8;
+            },
+            3 => self.sp = value,
+
+            _ => panic!("opcode segment should only be 3 bits wide"),
+        }
+    }
+
+    fn get_r16_stk(&self, reg: u8) -> u16 {
+        match reg {
             0 => (self.b as u16) << 8 | self.c as u16,
             1 => (self.d as u16) << 8 | self.e as u16,
             2 => self.get_hl(),
@@ -98,10 +123,10 @@ impl Registers {
         }
     }
 
-    fn get_r16_mem(&self, opcode: u8) -> u16 {
+    fn get_r16_mem(&self, reg: u8) -> u16 {
         let hl = self.get_hl();
 
-        match opcode & 0x3 {
+        match reg {
             0 => (self.b as u16) << 8 | self.c as u16,
             1 => (self.d as u16) << 8 | self.e as u16,
             2 => hl + 1,
@@ -145,6 +170,8 @@ impl CPU {
         let opcode = ram[self.regs.pc];
         let block = opcode >> 6;
 
+        self.regs.pc += 1;
+        
         match block {
             0x0 => self.execute_block_0_opcode(opcode, ram),
             0x1 => self.execute_block_1_opcode(opcode, ram),
@@ -156,7 +183,125 @@ impl CPU {
     }
     
     fn execute_block_0_opcode(&mut self, opcode: u8, ram: &mut RAM) {
+        if opcode == 0x00 {
+            return; // nop
+        }
 
+        {
+            let r16 = (opcode & 0x30) >> 4;
+            let imm16 = (ram[self.regs.pc + 1] as u16) << 8 | ram[self.regs.pc] as u16;
+
+            match opcode & 0xF {
+                0x1 => {
+                    // ld r16, imm16
+                    self.regs.set_r16(r16, imm16);
+                    self.regs.pc += 2;
+                    return;
+                },
+                0x2 => {
+                    // ld [r16mem], a
+                    let addr = self.regs.get_r16_mem(r16);
+                    ram[addr] = self.regs.a;
+                    return;
+                },
+                0xA => {
+                    // ld a, [r16mem]
+                    let addr = self.regs.get_r16_mem(r16);
+                    self.regs.a = ram[addr];
+                    return;
+                },
+
+                0x3 => {
+                    // inc r16
+                    self.regs.set_r16(r16, self.regs.get_r16(r16) + 1);
+                    return;
+                },
+                0xB => {
+                    // dec r16
+                    self.regs.set_r16(r16, self.regs.get_r16(r16) - 1);
+                    return;
+                },
+                0x9 => {
+                    // add hl, r16
+                    self.regs.set_hl(self.regs.get_hl() + self.regs.get_r16(r16));
+                    return;
+                },
+                _ => {},
+            };
+
+            if opcode == 0x08 {
+                // ld [imm16], sp
+                ram[imm16] = (self.regs.sp & 0xFF) as u8;
+                ram[imm16 + 1] = (self.regs.sp >> 8) as u8;
+                self.regs.pc += 2;
+                return;
+            }
+        }
+
+        {
+            let r8 = (opcode & 0x30) >> 4;
+            match opcode & 0x3 {
+                0x4 => {
+                    // inc r8
+                    self.regs.set_r8(r8, self.regs.get_r8(r8, ram) + 1, ram);
+                    return;
+                },
+                0x5 => {
+                    // dec r8
+                    self.regs.set_r8(r8, self.regs.get_r8(r8, ram) - 1, ram);
+                    return;
+                },
+                0x6 => {
+                    // ld r8, imm8
+                    self.regs.set_r8(r8, ram[self.regs.pc], ram);
+                    self.regs.pc += 1;
+                    return;
+                },
+                _ => {},
+            };
+        }
+        
+        match opcode {
+            0x07 => {
+                return;
+            },
+            0x0F => {
+                return;
+            },
+            0x17 => {
+                return;
+            },
+            0x1F => {
+                return;
+            },
+            0x27 => {
+                return;
+            },
+            0x2F => {
+                return;
+            },
+            0x37 => {
+                return;
+            },
+            0x3F => {
+                return;
+            },
+            _ => {},
+        };
+
+        if opcode == 0x18 {
+            return;
+        }
+
+        if opcode & 0x27 == 0x20 {
+            return;
+        }
+
+        if opcode == 0x10 {
+            return;
+        }
+
+        panic!("Invalid opcode: {opcode}");
     }
 
     fn execute_block_1_opcode(&mut self, opcode: u8, ram: &mut RAM) {
