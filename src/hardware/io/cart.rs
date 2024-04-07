2f2e3eadeb3045a2ef2e9be3596f70e4a905aab2
Sammy Humphreys <Sammyh2009@outlook.com>
2024-04-07 12:57:51 +0100

    add mbc3 again in the new format

--- src/hardware/io/cart.rs
+++ src/hardware/io/cart.rs
@@ -18,7 +18,7 @@ trait MBC: std::fmt::Debug {
 
 #[derive(Debug)]
 struct MBC1 {
-    ram_enable: bool,
+    ram_enabled: bool,
     rom_bank: u8,
     ram_bank: u8,
     total_rom_banks: u8,
@@ -34,7 +34,7 @@ impl MBC for MBC1 {
         let ram = vec![0; ram_bytes];
 
         Self {
-            ram_enable: false,
+            ram_enabled: false,
             rom_bank: 0,
             ram_bank: 0,
             total_rom_banks: rom_banks as u8,
@@ -69,7 +69,7 @@ impl MBC for MBC1 {
         match address {
             0x0000..=0x1FFF => {
                 // RAM enable
-                self.ram_enable = value & 0xA == 0xA;
+                self.ram_enabled = value & 0xA == 0xA;
             }
             0x2000..=0x3FFF => {
                 let mut bank = value & 0x1F;
@@ -98,7 +98,7 @@ impl MBC for MBC1 {
     }
 
     fn read_ram(&self, address: u16) -> u8 {
-        if self.ram_enable {
+        if self.ram_enabled {
             let address = ((self.ram_bank as u16) << 12) | address;
             self.ram[address as usize]
         }
@@ -108,9 +108,115 @@ impl MBC for MBC1 {
     }
 
     fn write_ram(&mut self, address: u16, value: u8) {
-        if self.ram_enable {
+        if self.ram_enabled {
             let address = ((self.ram_bank as u16) << 12) | address;
             self.ram[address as usize] = value;
+        }
+    }
+}
+
+#[derive(Debug)]
+struct MBC3 {
+    rom_bank: u8,
+    ram_bank: u8,
+    total_rom_banks: u8,
+    total_ram_banks: u8,
+    rom: Vec<u8>,
+    ram: Vec<u8>,
+    ram_enabled: bool,
+}
+
+impl MBC for MBC3 {
+    fn from_cart_header(mbc_type: u8, rom_banks: usize, ram_banks: usize, rom: Vec<u8>) -> Self {
+        let ram_bytes = EIGHT_KILOBYTES * ram_banks;
+        let ram = vec![0; ram_bytes];
+
+        Self {
+            ram_enabled: false,
+            rom_bank: 0,
+            ram_bank: 0,
+            total_rom_banks: rom_banks as u8,
+            total_ram_banks: ram_banks as u8,
+            rom,
+            ram,
+        }
+    }
+
+    fn read_rom(&self, address: u16) -> u8 {
+        let bank = match address {
+            0x0000..=0x3FFF => 0,
+            0x4000..=0x7FFF => self.rom_bank,
+            _ => panic!("{address} not a valid ROM address")
+        };
+        let address = bank as usize * SIXTEEN_KILOBYTES + (address as usize & 0x3FFF);
+
+        self.rom[address]
+    }
+
+    fn write_rom(&mut self, address: u16, value: u8) {
+        match address {
+            0x0000..=0x1FFF => {
+                // RAM enable
+                self.ram_enabled = value & 0xA == 0xA;
+            }
+            0x2000..=0x3FFF => {
+                let mut bank = value & 0x8F;
+                if bank == 0 {
+                    bank = 1;
+                }
+                
+                let mask = (!self.total_rom_banks) as u8; // flip bits. e.g. 4 needs 2 bits so it goes to 0b11;
+                bank &= mask;
+                
+                self.rom_bank = bank;
+                println!("Switched to rom bank {bank}");
+            }
+            0x4000..=0x5FFF => {
+                self.ram_bank = value;
+                println!("Switched to ram bank {value}");
+            }
+            0x6000..=0x7FFF => {
+                // TODO: RTC
+                // would latch clock
+                warn!("RTC not implemented yet")
+            }
+            _ => panic!("{address} not a valid ROM address")
+        }
+    }
+
+    fn read_ram(&self, address: u16) -> u8 {
+        // TODO: RTC
+        if self.ram_enabled {
+            if self.ram_bank <= 0x03 {
+                let address = ((self.ram_bank as u16) << 12) | address;
+                self.ram[address as usize]
+            }
+            else if self.ram_bank >= 0x8 && self.ram_bank <= 0xC {
+                // TODO: RTC. should select a rtc register
+                warn!("RTC not implemented yet");
+                0xFF
+            }
+            else {
+                warn!("{} not valid", self.ram_bank);
+                0xFF
+            }
+        }
+        else {
+            0xFF
+        }
+    }
+
+    fn write_ram(&mut self, address: u16, value: u8) {
+        // TODO: RTC
+        if self.ram_enabled {
+            if self.ram_bank <= 0x3 {
+                let address = ((self.ram_bank as u16) << 12) | address;
+                self.ram[address as usize] = value;
+            }
+            else if self.ram_bank >= 0x8 && self.ram_bank <= 0xC {
+                // TODO: RTC. should select a rtc register
+                warn!("RTC not implemented yet");
+            }
         }
     }
 }
@@ -190,10 +296,14 @@ impl Cartridge {
     fn mbc_from_cart_header(mbc_type: u8, rom_banks: usize, ram_banks: usize, rom: Vec<u8>) -> Box<dyn MBC> {
         if mbc_type == 0x00 {
             Box::new(NoMBC::from_cart_header(mbc_type, rom_banks, ram_banks, rom))
-        } else if mbc_type >= 0x01 && mbc_type <= 0x03 {
+        } 
+        else if mbc_type >= 0x01 && mbc_type <= 0x03 {
             Box::new(MBC1::from_cart_header(mbc_type, rom_banks, ram_banks, rom))
-        // Add other conditions for different MBC types as needed
-        } else {
+        }
+        else if mbc_type >= 0x0F && mbc_type <= 0x13 {
+            Box::new(MBC1::from_cart_header(mbc_type, rom_banks, ram_banks, rom))
+        }
+        else {
             warn!("Unsopported MBC {mbc_type:02X}. Defaulting to no mbc");
             Box::new(NoMBC::from_cart_header(mbc_type, rom_banks, ram_banks, rom)) // Default case
         }
