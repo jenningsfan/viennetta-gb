d43a126061770805d05e6d5177d474dd043554e1
Sammy Humphreys <Sammyh2009@outlook.com>
2025-10-18 15:40:09 +0100

    RENAME webgpu to standalone

--- standalone/src/main.rs
+++ standalone/src/main.rs
@@ -43,7 +43,7 @@ fn main() -> Result<(), Error> {
     let window = {
         let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
         WindowBuilder::new()
-            .with_title("Hello Pixels")
+            .with_title("Vienetta")
             .with_inner_size(size)
             .with_min_inner_size(size)
             .build(&event_loop)
